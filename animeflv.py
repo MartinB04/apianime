c37476abd602a9d66fd58669b5689234bfd930d5
MartinB04 <martin.bibiano4@outlook.com>
2024-10-14 16:43:02 -0600

    Change: Extraccion de links agregada.

--- animeflv.py
+++ animeflv.py
@@ -2,7 +2,7 @@
 import requests
 from lxml import html
 
-encabezados = {"user-agents": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36"}
+encabezados = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36"}
 
 url = "https://www3.animeflv.net"
 
@@ -16,14 +16,16 @@ if respuesta.status_code == 200:
 
     nombres_animes = parser.xpath("//ul[@class='ListAnimes AX Rows A06 C04 D03']//article[contains(@class, 'Anime')]//h3[@class='Title']/text()")
     urls_imagenes = parser.xpath("//ul[@class='ListAnimes AX Rows A06 C04 D03']//article[@class='Anime alt B']//img/@src")
+    urls_links = parser.xpath("//ul[@class='ListAnimes AX Rows A06 C04 D03']//article[@class='Anime alt B']//a/@href")
 
     # urls_animes = parser.xpath("//div[@class='book-list']//a[@class='text-center']/img/@src")
 
 
     
-    for nombre, url_imagen in zip(nombres_animes, urls_imagenes):
-        print("Nombre del manga:", nombre)
+    for nombre, url_imagen, urls_links in zip(nombres_animes, urls_imagenes, urls_links):
+        print("Nombre del anime:", nombre)
         print("URL de la imagen:", url+url_imagen)
+        print("URL del link:", url+urls_links)
         
 
 else:
